0fb6dc225f7c306bae194222cf4a6952bb2df2e3
JudahSan <baraka.judah@yahoo.com>
2023-01-12 18:38:02 +0300

    clean code

--- myapp/views.py
+++ myapp/views.py
@@ -3,7 +3,6 @@ from .models import Food, Consume
 # Create your views here.
 
 def index(request):
-  user_food = Consume.objects.filter(user=request.user)
   if request.method == 'POST':
     food_consumed = request.POST['food_consumed']
     consume = Food.objects.get(name=food_consumed)
@@ -13,7 +12,6 @@ def index(request):
     foods = Food.objects.all()
   else :
     # Get items consumed by currently logged in user
-    user_food = Consume.objects.filter(user=request.user)
     foods = Food.objects.all()
-
+  user_food = Consume.objects.filter(user=request.user)
   return render(request, 'myapp/index.html', {'foods':foods, 'user_food': user_food})
